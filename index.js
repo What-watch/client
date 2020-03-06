eaefeaba7c20fb77ceee2926558499664aa134c3
tamarazu <tamarazulaika.u@gmail.com>
2020-03-06 10:07:24 +0700

    jquery for fetch data upcoming and now

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+function hideAll() {
+    $("#listMovies").hide()
+}
+
+function fetchMoviesNow() {
+    console.log('masuk fetch movies now')
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/movies/now'
+    })
+        .done(movies => {
+            console.log(movies)
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
+function fetchMoviesUpcoming() {
+    console.log('masuk fetch movies upcoming')
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/movies/now'
+    })
+        .done(movies => {
+            console.log(movies)
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
 $(document).ready( () => {
-    
+    hideAll()
+    fetchMoviesNow()
 })
